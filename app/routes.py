efb9ce3fa9a81ec028b8e2978c577c3c49e27689
payr0001 <alix.payraudeau@etudiant.univ-reims.fr>
2023-02-23 13:16:28 +0000

    mise en place des premi`eres routes

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+# app/routes.py
+from flask import render_template
+from app import app
+from app.models import Territoire, TypeTerritoire, Periode, InfosJob, InfosLogement
+
+@app.route('/')
+@app.route('/index')
+def index() -> str:
+    user = { 'username' : 'Philippe' }
+    return render_template('index.html', title='Page principale', user=user)
+
+
+@app.route('/territoires')
+def territoires():
+    territoires = Territoire.query.all()
+    return render_template('territoires.html', territoires=territoires)
+
+
+@app.route('/data')
+def data():
+    territoires = Territoire.query.all()
+    typeTerritoire = TypeTerritoire.query.all()
+    periode = Periode.query.all()
+    infosJob = InfosJob.query.all()
+    infosLogement = InfosLogement.query.all()
+    return render_template('data.html', territoires=territoires)
