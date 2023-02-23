28dc0488dab83e6a623d8b7b27b2950b2496db49
payr0001 <alix.payraudeau@etudiant.univ-reims.fr>
2023-02-23 18:17:14 +0000

    ajout des routes factices en révision des autres pages

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,7 @@ from app.models import Territoire, TypeTerritoire, Periode, InfosJob, InfosLogem
 @app.route('/')
 @app.route('/index')
 def index() -> str:
-    user = { 'username' : 'Philippe' }
-    return render_template('index.html', title='Page principale', user=user)
+    return render_template('index.html')
 
 
 @app.route('/territoires')
@@ -24,3 +23,21 @@ def data():
     infosJob = InfosJob.query.all()
     infosLogement = InfosLogement.query.all()
     return render_template('data.html', territoires=territoires, typeTerritoire=typeTerritoire, periode=periode, infosJob=infosJob, infosLogement=infosLogement)
+
+@app.route('/python')
+def python():
+    territoires = Territoire.query.all()
+    typeTerritoire = TypeTerritoire.query.all()
+    periode = Periode.query.all()
+    infosJob = InfosJob.query.all()
+    infosLogement = InfosLogement.query.all()
+    return render_template('python/python.html', territoires=territoires, typeTerritoire=typeTerritoire, periode=periode, infosJob=infosJob, infosLogement=infosLogement)
+
+@app.route('/powerBI')
+def powerBI():
+    territoires = Territoire.query.all()
+    typeTerritoire = TypeTerritoire.query.all()
+    periode = Periode.query.all()
+    infosJob = InfosJob.query.all()
+    infosLogement = InfosLogement.query.all()
+    return render_template('powerBI/powerBI.html', territoires=territoires, typeTerritoire=typeTerritoire, periode=periode, infosJob=infosJob, infosLogement=infosLogement)
