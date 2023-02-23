79ea1192eb63d64cd30d165dec06e513f0ff279d
payr0001 <alix.payraudeau@etudiant.univ-reims.fr>
2023-02-23 13:16:00 +0000

    mise en place des premiers modèles en lien avec la base de données

---
+++ app/models.py
@@ -0,0 +1,42 @@
+# app/models.py
+from app import db
+
+# Territoire
+class Territoire(db.Model):
+    __tablename__ = 'Territoire'
+    codeTerritoire = db.Column(db.String(3), primary_key=True)
+    libelleTerritoire = db.Column(db.String(100))
+    codeTypeTerritoire = db.Column(db.String(20), db.ForeignKey('TypeTerritoire.codeTypeTerritoire'), primary_key=True)
+
+# Type de Territoire
+class TypeTerritoire(db.Model):
+    __tablename__ = 'TypeTerritoire'
+    codeTypeTerritoire = db.Column(db.String(20), primary_key=True)
+    libelleTypeTerritoire = db.Column(db.String(200), unique=True)
+
+# Période
+class Periode(db.Model):
+    __tablename__ = 'Periode'
+    codePeriode = db.Column(db.String(200), primary_key=True)
+    libellePeriode = db.Column(db.String(200))
+
+# Information Job
+class InfosJob(db.Model):
+    __tablename__ = 'InfosJob'
+    codePeriode = db.Column(db.String(200), db.ForeignKey('Periode.codePeriode'), primary_key=True)
+    codeTerritoire = db.Column(db.String(200), db.ForeignKey('Territoire.codeTerritoire'), primary_key=True)
+    codeTypeTerritoire = db.Column(db.String(20), db.ForeignKey('TypeTerritoire.codeTypeTerritoire'), primary_key=True)
+    valeurIndic = db.Column(db.Float)
+    population = db.Column(db.Integer)
+
+# Information Logement
+class InfosLogement(db.Model):
+    __tablename__ = 'InfosLogement'
+    codePeriode = db.Column(db.String(200), db.ForeignKey('Periode.codePeriode'), primary_key=True)
+    codeTerritoire = db.Column(db.String(200), db.ForeignKey('Territoire.codeTerritoire'), primary_key=True)
+    codeTypeTerritoire = db.Column(db.String(20), db.ForeignKey('TypeTerritoire.codeTypeTerritoire'), primary_key=True)
+    nbLogements0VOIT = db.Column(db.Integer)
+    nbLogements1VOIT = db.Column(db.Integer)
+    nbLogements2VOIT = db.Column(db.Integer)
+    nbLogements3VOIT = db.Column(db.Integer)
+    nbLogementsAvecPlacesResa = db.Column(db.Integer)
