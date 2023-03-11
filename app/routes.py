61a2d6734a8b8865d5a8df328413cdef319272db
payr0001 <alix.payraudeau@etudiant.univ-reims.fr>
2023-03-11 14:20:24 +0000

    mise en place des premieres routes vers les tableaux de bord des departements et des regions

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app import app, bootstrap
 from app.models import Territoire, TypeTerritoire, Periode, InfosJob
 import plotly.express as px
 import pandas as pd
+from sqlalchemy import func
 
 @app.route('/')
 @app.route('/index')
@@ -31,18 +32,36 @@ def python():
     typeTerritoire = TypeTerritoire.query.all()
     periode = Periode.query.all()
     informations = InfosJob.query.all()
-    return render_template('python/.html', territoires=territoires, typeTerritoire=typeTerritoire, periode=periode, informations=informations, bootstrap=bootstrap)
+    return render_template('python/python.html', territoires=territoires, typeTerritoire=typeTerritoire, periode=periode, informations=informations, bootstrap=bootstrap)
 
 @app.route('/python/test')
 def pythontest():
     informations = InfosJob.query.all()
     df = pd.DataFrame.from_records([i.__dict__ for i in informations])
-    print(df.head())
     fig = px.box(df, x='codeTerritoire', y='population')
     graph_html = fig.to_html(full_html=False)
-    return render_template('python/pythontest.html', informations=informations, graph=graph_html, bootstrap=bootstrap)
+    return render_template('python/graphtest.html', informations=informations, graph_html=graph_html, bootstrap=bootstrap)
 
+@app.route('/python/<libelleTerritoire>')
+def territoire(libelleTerritoire: str) -> str:
+    territoire = Territoire.query.filter_by(libelleTerritoire=libelleTerritoire).first()
+    informations = InfosJob.query.filter_by(codeTerritoire=territoire.codeTerritoire, codeTypeTerritoire=territoire.codeTypeTerritoire).filter(func.length(InfosJob.codePeriode) == 4).all()    
+    df = pd.DataFrame.from_records([i.__dict__ for i in informations])
+    df['codePeriode'] = df['codePeriode'].astype(int)
 
+    #Création Graph
+    fig = px.bar(df.query('codePeriode >= 2018'), x='codePeriode', y='valeurIndic')
+    # Configuration Graph
+    fig.update_traces(legendgroup="", showlegend=True)
+    fig.update_layout(title="Valeur Indicateur - Dynamisme",
+                    legend=dict( yanchor="top", y=0.99, xanchor="left", x=0.01),
+                    xaxis_title="Période",
+                    yaxis_title="Valeur Indic",
+                    yaxis=dict(range=[0, 5]))
+
+    graph_valeurIndic = fig.to_html(full_html=False)
+
+    return render_template('python/territoire.html',territoire=territoire, informations=informations, graph_valeurIndic=graph_valeurIndic, bootstrap=bootstrap)
 
 @app.route('/powerBI')
 def powerBI():
